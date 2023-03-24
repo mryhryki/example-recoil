8c03796c36477118dcd428912e4f1123c0fe6c9e
mryhryki <mryhryki@gmail.com>
2023-03-24 17:33:13 +0900

    Update src/recoil/atoms/Count.ts

--- src/recoil/atoms/Count.ts
+++ src/recoil/atoms/Count.ts
@@ -1,8 +1,25 @@
-import {atom, useRecoilState} from "recoil";
+import {atom, AtomEffect, useRecoilState} from "recoil";
 
+const urlSyncEffect = (key: string): AtomEffect<number> => ({setSelf, onSet}) => () => {
+  setSelf(parseInt(new URL(location.href).searchParams.get(key) ?? '0') || 0)
+  onSet((newValue, _, isReset) => {
+    const url = new URL(location.href)
+    if (isReset) {
+      url.searchParams.delete(key)
+    } else {
+      url.searchParams.set(key, String(newValue))
+      history.replaceState(null, '', url)
+    }
+  })
+}
+
+const key = 'count'
 const countState = atom({
-    key: 'count',
-    default: 0,
+  key,
+  default: 0,
+  effects: [
+    urlSyncEffect(key)
+  ]
 })
 
 export const useCountState = () => useRecoilState(countState)
