658f9153d41314a81c7da7d4bb50048e3ee8b195
mryhryki <mryhryki@gmail.com>
2023-03-24 17:41:09 +0900

    Refactoring

--- src/recoil/atoms/TodoList.ts
+++ src/recoil/atoms/TodoList.ts
@@ -11,7 +11,7 @@ const todoListState = atom<Todo[]>({
   key,
   default: [],
   effects: [
-    localStorageEffect('TodoList'),
+    localStorageEffect(key),
   ]
 })
 
--- src/recoil/effects/urlSyncEffect.ts
+++ src/recoil/effects/urlSyncEffect.ts
@@ -1,15 +1,18 @@
 import {AtomEffect} from "recoil";
 
-export const urlSyncEffect = (key: string): AtomEffect<number> => ({setSelf, onSet}) => () => {
-  setSelf(parseInt(new URL(location.href).searchParams.get(key) ?? '0') || 0)
+export const urlSyncEffect = <T>(key: string): AtomEffect<T> => ({setSelf, onSet}) => () => {
+  const urlValue = new URL(location.href).searchParams.get(key)
+  if (urlValue != null) {
+    setSelf(JSON.parse(urlValue))
+  }
   onSet((newValue, _, isReset) => {
     const url = new URL(location.href)
     if (isReset) {
       url.searchParams.delete(key)
     } else {
-      url.searchParams.set(key, String(newValue))
-      history.replaceState(null, '', url)
+      url.searchParams.set(key, JSON.stringify(newValue))
     }
+    history.replaceState(null, '', url)
   })
 }
 
